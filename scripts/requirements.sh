958bb22e1df7f9825b347b7b098dc7da96e97e6d
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-04-02 16:48:41 -0400

    added requirements bash script

---
+++ scripts/requirements.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt-get update
+
+# Requirements for opencv / cv2
+sudo apt-get install libxrender-dev
+sudo apt-get install libsm6
+
+# Requirements for pymatbridge
+sudo apt-get install libxext6
